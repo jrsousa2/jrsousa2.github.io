0d09b6ddacd0f6aef4381f073970568a57dcebf6
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-05-24 16:03:51 -0300

    Updt

---
+++ GitPage.bat
@@ -0,0 +1,29 @@
+@echo off
+REM Check if the argument %1 is provided
+
+IF "%~1"=="" (
+    echo Setting description to Updt
+    echo .
+    set Desc=Updt
+) ELSE (
+  set Desc=%1
+)
+
+
+ cd D:\GH
+ set GIT_TRACE=1
+ echo FORCE A RE-READING OF THE .gitignore list
+ git rm -r --cached .
+ git add .
+ git branch -M main
+ git commit -m %Desc%
+ REM COMMENTED OUT CODE FORCES A PUSH
+ REM It loses track of remote updates
+ REM git push origin main --force
+ git push origin main
+ REM echo sem nada mostra o echo status
+ echo .
+ echo .
+ echo VIEWS IF BATCH SUCCEEDED
+ git log -n 1
+
