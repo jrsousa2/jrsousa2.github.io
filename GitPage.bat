f4c74bbb1e2975522b4e570862b175decba0be8f
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-05-24 16:48:33 -0300

    Updt

--- GitPage.bat
+++ GitPage.bat
@@ -22,12 +22,13 @@ REM git branch -M main
  REM git push origin main --force
  git push origin main
  REM echo sem nada mostra o echo status
- echo .
- echo .
- echo VIEWS IF BATCH SUCCEEDED (USING "git log -n 1" IS NOT TOO USEFUL)
+ echo.
+ echo.
+ echo VIEWS IF BATCH SUCCEEDED 
+REM USING "git log -n 1" IS NOT TOO USEFUL
  git log -1 --name-status 
- echo .
- echo .
+ echo.
+ echo.
  echo VIEWS STATUS
  git status
  
