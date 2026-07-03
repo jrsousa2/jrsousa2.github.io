80724f32b012da197b622ad8b2aaca0ff0e8c6d2
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-07-03 12:51:12 -0300

    Updt

--- GitPage.bat
+++ GitPage.bat
@@ -16,7 +16,7 @@ IF "%~1"=="" (
  cd D:\GH
  set GIT_TRACE=1
  echo TO FORCE A RE-READING OF THE .gitignore list uncomment the next line
- REM git rm -r --cached .
+ git rm -r --cached .
  git add -A
  REM git branch -M main
  git commit -m "%Desc%"
